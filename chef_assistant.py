f4c3113a91d334e7bbe46c316cb0507a284aea96
Valent100000 <90402755+Valent100000@users.noreply.github.com>
2021-12-16 12:03:17 +0300

    Add files via upload

--- chef_assistant.py
+++ chef_assistant.py
@@ -10,7 +10,6 @@ def choose (entry, entry2, entry3):
 
     def choose_recipes(choose_meals, consumers):
 
-
         trouble = []
         part = []
         full = []
@@ -25,12 +24,16 @@ def choose (entry, entry2, entry3):
             for pr in a:
                 b=(pr["Продукт"])
                 kol = products.search(where("Продукт") == b)
+                if kol:
+                    if (not float(pr["Объём"]) * consumers <= float(kol[0]["Объём"])) and pr["Статус"] == "Обязательно":
+                        flag = 2
+                    elif (not float(pr["Объём"]) * consumers <= float(kol[0]["Объём"])) and pr[
+                        "Статус"] == "Необязательно" and flag != 2:
+                        flag = 1
+                        trouble.append(pr["Продукт"])
+                else:
+                    flag = 3
 
-                if (not float(pr["Объём"])*consumers<=float(kol[0]["Объём"])) and pr["Статус"] == "Обязательно":
-                    flag=2
-                elif (not float(pr["Объём"])*consumers<=float(kol[0]["Объём"])) and pr["Статус"] == "Необязательно" and flag!=2:
-                    flag=1
-                    trouble.append(pr["Продукт"])
             if flag == 0:
                 full.append(meal + " (FULL)")
             if flag  == 1:
