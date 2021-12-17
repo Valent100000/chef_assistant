b0d4ae23a23eb9c2f69e0fd0c6732c133181b040
Valent100000 <90402755+Valent100000@users.noreply.github.com>
2021-12-17 09:06:10 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,22 +1,21 @@
 def refill_recipe_database(meal, massive_ingridients):
-    '''function of replenishing the recipe database'''
     import tinydb
     from tinydb import TinyDB, Query
     db = tinydb.TinyDB('recipes.db')
     Ingredient = Query()
     for position in massive_ingridients:
         db.insert({'Блюдо': meal, 'Продукт': position[0], 'Объём': position[1], 'Единицы': position[2], 'Статус': position[3]})
+    db.close()
 
 def refill_product_database(product, amount, units):
-    '''function to replenish the database warehouse'''
     import tinydb
     from tinydb import TinyDB, Query
     db_1 = tinydb.TinyDB('products.db')
     Product = Query()
     db_1.insert({'Продукт': product, 'Объём': amount, 'Единицы': units})
+    db_1.close()
 
 def refill_characteristic_database(meal, kitchen, time):
-    '''function to replenish the database of characteristic'''
     import tinydb
     from tinydb import TinyDB, Query
     db_2 = tinydb.TinyDB('characteristic.db')
@@ -24,4 +23,5 @@ def refill_characteristic_database(meal, kitchen, time):
     kitchen = kitchen.split()
     time = time.split()
     db_2.insert({'Блюдо': meal, 'Кухни': kitchen, 'Приём пищи': time})
+    db_2.close()
 
