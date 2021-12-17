ec6d967e082c2f519c8539282f417484b5ff2ec3
Valent100000 <90402755+Valent100000@users.noreply.github.com>
2021-12-17 22:03:11 +0300

    Update database.py
    correct

--- database.py
+++ database.py
@@ -7,8 +7,9 @@ def refill_recipe_database(meal, massive_ingridients):
     from tinydb import TinyDB, Query
     db = tinydb.TinyDB('recipes.db')
     Ingredient = Query()
-    for position in massive_ingridients:
-        db.insert({'Блюдо': meal, 'Продукт': position[0], 'Объём': position[1], 'Единицы': position[2], 'Статус': position[3]})
+    if meal and massive_ingridients:
+        for position in massive_ingridients:
+            db.insert({'Блюдо': meal, 'Продукт': position[0], 'Объём': position[1], 'Единицы': position[2], 'Статус': position[3]})
     db.close()
 
 def refill_product_database(product, amount, units):
@@ -20,7 +21,8 @@ def refill_product_database(product, amount, units):
     from tinydb import TinyDB, Query
     db_1 = tinydb.TinyDB('products.db')
     Product = Query()
-    db_1.insert({'Продукт': product, 'Объём': amount, 'Единицы': units})
+    if product and amount and units:
+        db_1.insert({'Продукт': product, 'Объём': amount, 'Единицы': units})
     db_1.close()
 
 def refill_characteristic_database(meal, kitchen, time):
@@ -28,15 +30,16 @@ def refill_characteristic_database(meal, kitchen, time):
    function to replenish the database of goods by characteristics of the kitchen and the time of food intake\n
    функция для пополнения базы данных товаров по характеристикам кухня и время приема пищи
     """
-    import tinydb
+   import tinydb
     from tinydb import TinyDB, Query
     db_2 = tinydb.TinyDB('characteristic.db')
     Meals = Query()
     kitchen = kitchen.split()
     time = time.split()
-    db_2.insert({'Блюдо': meal, 'Кухни': kitchen, 'Приём пищи': time})
+    if meal and kitchen and time:
+        db_2.insert({'Блюдо': meal, 'Кухни': kitchen, 'Приём пищи': time})
     db_2.close()
-
+    
 def delete_product(product):
     """
     function of delete the product from the database\n
@@ -46,7 +49,8 @@ def delete_product(product):
     from tinydb import TinyDB, Query, where
     db_1 = tinydb.TinyDB('products.db')
     Product = Query()
-    db_1.remove(where('Продукт') == product)
+    if product:
+        db_1.remove(where('Продукт') == product)
     db_1.close()
 
 def delete_recipe(meal):
@@ -58,9 +62,12 @@ def delete_recipe(meal):
     from tinydb import TinyDB, Query, where
     db = tinydb.TinyDB('recipes.db')
     Resipe = Query()
-    db.remove(where('Блюдо') == meal)
+    if meal:
+        db.remove(where('Блюдо') == meal)
     db_2 = tinydb.TinyDB('characteristic.db')
     Meals = Query()
-    db_2.remove(where('Блюдо') == meal)
+    if meal:
+        db_2.remove(where('Блюдо') == meal)
     db.close()
     db_2.close()
+
